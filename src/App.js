89b1a2a9d26d8fa942879d11dfa9a9949ab3988b
Kimia <kimia.rfp@gmail.com>
2023-06-12 22:49:31 +0330

    use Tailwind

--- src/App.js
+++ src/App.js
@@ -12,15 +12,31 @@ export default function App() {
   const [password, setPassword] = useState("Password Generator");
   const [star, setStar] = useState("text");
   const [eye, setEye] = useState(
-    <FontAwesomeIcon icon={faEye} onClick={dontShowPass} />
+    <FontAwesomeIcon
+      icon={faEye}
+      onClick={dontShowPass}
+      title="Hide password"
+    />
   );
 
   function dontShowPass() {
-    setEye(<FontAwesomeIcon icon={faEyeSlash} onClick={showPass} />);
+    setEye(
+      <FontAwesomeIcon
+        icon={faEyeSlash}
+        onClick={showPass}
+        title="Show password"
+      />
+    );
     setStar("password");
   }
   function showPass() {
-    setEye(<FontAwesomeIcon icon={faEye} onClick={dontShowPass} />);
+    setEye(
+      <FontAwesomeIcon
+        icon={faEye}
+        onClick={dontShowPass}
+        title="Hide password"
+      />
+    );
     setStar("text");
   }
 
@@ -43,14 +59,19 @@ export default function App() {
 
   return (
     <div>
-      <input type={star} value={password} readOnly />
-      <div className="icons">
-        <button>{eye}</button>
-        <button onClick={generatePass}>
-          <FontAwesomeIcon icon={faRotate} />
+      <input
+        type={star}
+        value={password}
+        readOnly
+        className="text-xl px-4 lg:w-2/5 sm:w-3/5 h-20 shadow-lg absolute left-2/4 top-2/4 rounded-xl select-none outline-none -translate-y-2/4 -translate-x-2/4"
+      />
+      <div className="icons absolute left-2/4 bottom-1/3 sm:-translate-y-24 lg:translate-x-28 sm:translate-x-24  -translate-x-16">
+        <button className="text-2xl p-1">{eye}</button>
+        <button className="text-2xl p-1" onClick={generatePass}>
+          <FontAwesomeIcon icon={faRotate} title="Create Password" />
         </button>
-        <button onClick={copyPass}>
-          <FontAwesomeIcon icon={faClipboard} />
+        <button className="text-2xl p-1" onClick={copyPass}>
+          <FontAwesomeIcon icon={faClipboard} title="Copy to clipboard" />
         </button>
       </div>
       <ToastContainer />
